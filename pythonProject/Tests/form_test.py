32d44b38823780edf76fb4e7b7398fc386b93df2
volodymyrrrr <focuswow34@gmail.com>
2023-06-22 16:52:58 +0300

    Update form_test.py

--- pythonProject/Tests/form_test.py
+++ pythonProject/Tests/form_test.py
@@ -16,7 +16,7 @@ client = gspread.service_account('pythonProject/Tests/gs_credentials.json')
 working_sheet = client.open_by_url(
     'https://docs.google.com/spreadsheets/d/1fRi9qAdb-E-xAY_jQiMdjjEsN1xZZdxK6865V-Ck6RE/edit#gid=0')
 wb1 = working_sheet.get_worksheet(0)
-URLS = wb1.get_values('B2:G500')
+URLS = wb1.get_values('B2:H500')
 if os.path.exists("links.csv"):
     os.remove("links.csv")
 else:
